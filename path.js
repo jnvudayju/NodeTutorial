61d85e1fc4fa557a8f5aff8c447e486a22a21c2a
jnvudayju <63098026+jnvudayju@users.noreply.github.com>
2023-07-22 09:54:39 +0530

    Delete path.js

--- path.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-console.log(path.dirname('C:/Users/amit kumar sharma/Nodejst/PathModule/path.js'));
-console.log(path.extname('C:/Users/amit kumar sharma/Nodejst/PathModule/path.js'));
-console.log(path.basename('C:/Users/amit kumar sharma/Nodejst/PathModule/path.js'));
-console.log(path.parse('C:/Users/amit kumar sharma/Nodejst/PathModule/path.js'));
-
-const myPath = path.parse('C:/Users/amit kumar sharma/Nodejst/PathModule/path.js');
-console.log(myPath.name);
-console.log(myPath.root);
